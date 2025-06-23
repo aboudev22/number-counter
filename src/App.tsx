15e4fe06459b001407c0eb0f87fe99619773a668
aboudev22 <aboubakarjay2@gmail.com>
2025-06-23 18:36:53 +0100

    counter jun 23

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,86 @@
-import clsx from "clsx";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const [yValue, setYValue] = useState(0);
+  const [digits, setDigits] = useState<number[]>([0]);
+  const [isNegative, setIsNegative] = useState(false);
+
+  const updateDigit = (newCompte: number) => {
+    const newDigit = Math.abs(newCompte)
+      .toString()
+      .split("")
+      .reverse()
+      .map(Number);
+    setDigits(newDigit);
+    setIsNegative(newCompte < 0);
+  };
+
   const incremente = () => {
+    const newCompte = count + 1;
     setCount(count + 1);
-    setYValue(yValue === 900 ? 0 : yValue + 100);
+    updateDigit(newCompte);
   };
   const decremente = () => {
+    const newCompte = count - 1;
     setCount(count - 1);
-    setYValue(yValue === 0 ? 900 : yValue - 100);
+    updateDigit(newCompte);
   };
 
   return (
-    <div className="w-screen h-screen bg-neutral-900 flex justify-center items-center">
-      <div className="flex items-center gap-2 bg-neutral-800 p-3 rounded-full overflow-hidden">
-        <button
-          onClick={decremente}
-          className="w-10 h-10 rounded-full text-2xl text-white font-bold bg-amber-950 cursor-pointer active:scale-90 transition-all will-change-transform"
-        >
-          -
-        </button>
-        {Array.from({ length: count.toString().length }).map((_, i) => (
-          <Number key={i} y={yValue} />
-        ))}
-        <button
-          onClick={incremente}
-          className="w-10 h-10 rounded-full text-2xl text-white font-bold bg-amber-950 cursor-pointer active:scale-90 transition-all will-change-transform"
-        >
-          +
-        </button>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex gap-4 items-center justify-center">
+        <Button onClick={decremente} label="Decremente" />
+        <AnimatePresence>
+          {isNegative && (
+            <p className="text-6xl h-14 text-pink-500 font-extrabold">-</p>
+          )}
+        </AnimatePresence>
+        <div className="flex flex-row-reverse">
+          {digits.map((value, i) => (
+            <TabDigit key={i} digit={value} />
+          ))}
+        </div>
+        <Button onClick={incremente} label="Incremente" />
       </div>
     </div>
   );
 }
 
-function Number({ y }: { y: number }) {
+function Button({
+  label,
+  onClick,
+}: {
+  label: "Incremente" | "Decremente";
+  onClick: () => void;
+}) {
   return (
-    <div className="flex flex-col h-8 overflow-hidden">
-      {Array.from({ length: 10 }).map((_, n) => (
-        <motion.p
-          animate={{ y: `-${y}%` }}
-          className={clsx("text-white text-2xl font-bold")}
-        >
-          {n}
-        </motion.p>
-      ))}
-    </div>
+    <button
+      onClick={onClick}
+      className="bg-black flex-none h-fit rounded-md text-pink-500  text-xs p-2 font-bold cursor-pointer active:scale-95 transition-all will-change-transform"
+    >
+      {label}
+    </button>
+  );
+}
+
+function TabDigit({ digit }: { digit: number }) {
+  return (
+    <motion.div
+      initial={{ x: 10, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      className="h-14 w-12 border-2 relative"
+    >
+      <motion.div
+        animate={{ y: `-${digit * 3.5}rem` }}
+        className="flex left-1/2 -translate-x-[50%] justify-center flex-col absolute"
+      >
+        {Array.from({ length: 10 }).map((_, n) => (
+          <p key={n} className="text-6xl h-14 text-pink-500 font-extrabold">
+            {n}
+          </p>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 }
